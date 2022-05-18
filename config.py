b9440676a5cc7dbfd4967569f1434d6b476624b4
beardmikle <ankmo@yandex.ru>
2022-05-18 18:03:05 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+import requests
+open_weather_token = "2982914f7b0e07e97009612970071c68"
+
+def get_weather(city, open_wearher_toke):
+    pass
+
+def main():
+    city = input("Введите город для запроса:")
+    get_weather(city, open_weather_token)
+
+if __name__ == '__main__':
+    main()
